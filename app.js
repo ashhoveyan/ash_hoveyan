35427f1eb4f464d08211595d3ee04fab14284096
Ashot Hoveyan <ash.hoveyan.02@mai.ru>
2024-08-14 20:46:22 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+import 'dotenv/config.js';
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import indexRouter from './routes/index.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app
