aec470743da452e564ac1367ab0ee513a13c2f96
Ashot Hoveyan <ash.hoveyan.02@mai.ru>
2024-08-14 21:41:57 +0400

    fixed some errors

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 import 'dotenv/config.js';
-import createError from 'http-errors';
-import express from 'express';
-import path from 'path'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
+import './initTables.js';
 
-import indexRouter from './routes/index.js';
+import path from "path";
+import express from "express";
+import logger from 'morgan';
+import createError from "http-errors";
+import cookieParser from "cookie-parser";
+
+import indexRouter from "./routes/index.js";
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.set('views', path.resolve('./views'))
+app.set('view engine', 'ejs')
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(logger('dev'))
+app.use(express.json())
 
-app.use(logger('dev'));
-app.use(express.json());
-
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.resolve('./public')))
 
 app.use('/', indexRouter);
 
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
+// error handler
 app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = err;
 
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
+
 
 export default app
