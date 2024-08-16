cdcdcbb4c1f3db2e7b3e655131c0e431639c4655
Ashot Hoveyan <ash.hoveyan.02@mai.ru>
2024-08-16 19:20:41 +0400

    commit

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,29 @@ import userSchema from '../schemas/users.js'
 import express from 'express'
 const router = express.Router()
 
-//views
+
+
 router.get('/registration', (req, res) => {
   res.render('registration')
 })
+
+
+router.post(
+    '/registration',
+    validate(userSchema.registration, 'body'),
+    controller.registration
+)
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+
+router.post('/login', validate(userSchema.login, 'body'), controller.login)
+
+router.get('/getUsersList', checkToken, controller.getUsersList)
+
+
 router.get('/profile', (req, res) => {
   res.render('profile')
 })
@@ -22,14 +38,8 @@ router.get('/updateUserProfile', (req, res) => {
   res.render('showUpdateUserProfile')
 })
 
-//Api
 
-router.post(
-    '/registration',
-    validate(userSchema.registration, 'body'),
-    controller.registration
-)
-router.post('/login', validate(userSchema.login, 'body'), controller.login)
+
 router.get('/getUsersList', checkToken, controller.getUsersList)
 router.get('/getUserProfile', checkToken, controller.getUserProfile)
 router.put(
