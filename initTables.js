02d6d25fcb1a99d905c59b3d57b0f67b522a2824
Ashot Hoveyan <ash.hoveyan.02@mai.ru>
2024-08-19 15:51:00 +0400

    fixet init table future

--- initTables.js
+++ initTables.js
@@ -9,18 +9,28 @@ async function main() {
             first_name varchar(50) not null ,
             last_name varchar(50) not null ,
             email varchar(100) not null unique ,
-            password  VARCHAR(50)
+            password  VARCHAR(50) not null
         )
     `);
     await db.query(`
-    CREATE TABLE IF NOT EXISTS posts (
-      id BIGINT AUTO_INCREMENT PRIMARY KEY,
-      title VARCHAR(255) NOT NULL,
-      description TEXT NOT NULL,
-      user_id BIGINT NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES users(id)
-    )
-  `);
+            CREATE TABLE IF NOT EXISTS posts (
+              id BIGINT AUTO_INCREMENT PRIMARY KEY,
+              title VARCHAR(255) NOT NULL,
+              description TEXT NOT NULL,
+              user_id BIGINT NOT NULL,
+              FOREIGN KEY (user_id) REFERENCES users(id)
+            )
+    `);
+    await db.query(`
+            CREATE TABLE IF NOT EXISTS comments (
+              id BIGINT AUTO_INCREMENT PRIMARY KEY,
+              comment_content VARCHAR(255) NOT NULL,
+              user_id BIGINT NOT NULL,
+              post_id BIGINT NOT NULL,
+              FOREIGN KEY (user_id) REFERENCES users(id),
+              FOREIGN KEY (post_id) REFERENCES posts(id)
+            )
+    `);
 }
 main().catch((err) => {
     console.log(err);
